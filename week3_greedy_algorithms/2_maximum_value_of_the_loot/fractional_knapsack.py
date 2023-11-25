82ba2ccb267b8deef984961a05690197b6cf7085
Rachana <rachanaa.v.01@gmail.com>
2023-11-25 09:07:21 +0530

    works now

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -4,19 +4,15 @@ from sys import stdin
 def optimal_value(capacity, weights, values):
     value = 0
     pervalue=[]
-
-    while capacity!=0:
-        for i in range(len(weights)):
+    for i in range(len(weights)):
             pervalue.append(values[i]/weights[i])
+    while capacity!=0:
         m = pervalue.index(max(pervalue))
         amount = min(capacity,weights[m])
-        value+=amount[m]*(pervalue[m])
+        value+=amount*(pervalue[m])
         pervalue[m]=0
         capacity-=amount
             
-
-
-
     return value
 
 
